978ebe3dabf5a5886e91a3b4fb185488d534ad59
Andrew McPherson <ehgoodenough@gmail.com>
2026-07-09 13:43:20 -0700

    Began adding multi-website support for beacon.

--- grabber.js
+++ grabber.js
@@ -3,28 +3,47 @@
 // You'll need to have chrome://flags/#canvas-draw-element enabled!!
 
 function snapshot() {
-    const video = document.getElementsByTagName("video")[0]
-    if(video == undefined) throw new Error("Could not find video")
-    video.bounds = video?.getBoundingClientRect()
+    let video = undefined
+    let subtitles = undefined
 
-    const subtitles = document.getElementById("ytp-caption-window-container")?.cloneNode(true)
+    if(window.location.host.includes("youtube.com")) {
+        video = document.getElementsByTagName("video")[0]
+        subtitles = document.getElementById("ytp-caption-window-container")?.cloneNode(true)
+    } else if(window.location.host.includes("dropout.tv")) {
+        // video = document.getElementsByTagName("video")
+        // Object.values(document.querySelectorAll("iframe")).forEach((iframe) => {
+        //     const subdocument = iframe.contentDocument || iframe.contentwindow?.document
+        //     if(subdocument?.getElementsByTagName("video")[0])
+        //     video = subdocument?.getElementsByTagName("video")[0] || video
+        //     subtitles = subdocument?.getElementsByClassName("vp-captions")[0] || subtitles
+        // })
+    } else if(window.location.host.includes("beacon.tv")) {
+        video = document.getElementsByTagName("video")[0]
+        subtitles = document.getElementsByClassName("jw-captions")[0]?.cloneNode(true)
+    } else {
+        throw new Error("This website is not supported for screengrabs.")
+    }
+
+
+
+    if(video == undefined) throw new Error("Could not find video")
+    const bounds = video?.getBoundingClientRect()
 
     // CANVAS //
 
     const canvas = document.createElement("canvas")
     canvas.setAttribute("layoutsubtree", true)
 
-    canvas.width = video.bounds.width
-    canvas.height = video.bounds.height
-    canvas.style.width = video.bounds.width + "px"
-    canvas.style.height = video.bounds.height + "px"
-    canvas.style.transform = "scale(1%)"
-    canvas.style.transformOrigin = "bottom left"
-
+    canvas.width = bounds.width
+    canvas.height = bounds.height
+    canvas.style.width = bounds.width + "px"
+    canvas.style.height = bounds.height + "px"
+    canvas.style.transformOrigin = "top right"
+    canvas.style.transform = "scale(10%)"
     canvas.style.position = "fixed"
-    canvas.style.zIndex = 99999
-    canvas.style.bottom = 0
-    canvas.style.left = 0
+    canvas.style.zIndex = 999999999
+    canvas.style.right = 0
+    canvas.style.top = 0
 
     if(subtitles != undefined) canvas.appendChild(subtitles)
     document.body.appendChild(canvas)
@@ -40,7 +59,9 @@ function snapshot() {
             await navigator.clipboard.write([
                 new ClipboardItem({"image/png": blob})
             ])
-            document.body.removeChild(canvas)
+            window.setTimeout(function() {
+                document.body.removeChild(canvas)
+            }, 1000)
         }, "image/png")
     })
 
